94e248a6a373c8493bf67a76ec108d30fd72d24a
Jauregui <israelgarciayael@gmail.com>
2021-09-05 01:39:26 -0500

    Version final

--- JS/ADC.js
+++ JS/ADC.js
@@ -1,3 +1,6 @@
+
+var botonon = document.getElementById('iniciar');
+
 class ADC{
     constructor(bits, reloj, tipo, vref){
         this.bits = bits;
@@ -6,16 +9,18 @@ class ADC{
         this.vref=vref;
         this.resolucion = this.vref/(Math.pow(2,this.bits)+1);
         this.error = this.resolucion/2;
-        if (tipo == 'Aprox'){
-            this.tconver = (this.bits+1)*(1/this.reloj);
+        if (tipo == 'rampa')
+        {
+            this.tconver = Math.pow(2,this.bits)*(1/this.reloj);    
         } 
         else{
-            this.tconver = Math.pow(2,this.bits)*(1/this.reloj);
+
+            this.tconver = (this.bits+1)*(1/this.reloj);
         }
     }
     Dato10(Va){
         let dato = Va/this.resolucion;
-        return dato;
+        return Math.round(dato);
     }
     bitsPerSeconds(fsa){
         let bps = fsa * this.bits;
@@ -26,3 +31,50 @@ class ADC{
         return MB;  
     }
 }
+
+const boton = document.querySelector("#iniciar");
+
+boton.addEventListener("click", function(){
+    
+    botonon.classList.toggle('on');
+    iniciar();
+    $(id = "#results").toggle();
+    $(id = "#parametros").toggle();
+});
+
+
+
+$("#Ventrada").on("change keyup paste", function(){
+    iniciar();
+})
+
+function iniciar(){
+    let tipo = document.getElementById('ADC').value;
+    let bits = document.getElementById('bits').value;
+    let vref = document.getElementById('Vref').value;
+    let orden = document.getElementById('orden').value;
+    let Formatiempo = document.getElementById('Formatiempo').value;
+    let ventrada = document.getElementById('Ventrada').value;
+    let frec = document.getElementById('frec').value;
+    let Tiempo = document.getElementById('tiempo').value;
+    if (orden == 'kilos')
+    {
+        frec = frec * 1000;
+    }
+    if (orden == 'Megas')
+    {
+        frec= frec * 1000000;
+    }
+    if (Formatiempo == 'Minutos')
+    {
+        Tiempo = Tiempo * 60;
+    }
+    const A = new ADC(bits,frec,tipo,vref);
+    document.getElementById('datodec').innerHTML = "Dato de salida en Decimal = " + A.Dato10(ventrada);
+    document.getElementById('datobin').innerHTML = "Dato de salida en Binario = " + A.Dato10(ventrada).toString(2);
+    document.getElementById('tconversion').innerHTML = "Tiempo de conversión = " + A.tconver + " segundos";
+    document.getElementById('bps').innerHTML = "Bits/segundo = " + A.bitsPerSeconds(frec)+ " bps";
+    document.getElementById('espacio').innerHTML = "Espacio = " + A.twr(A.bitsPerSeconds(frec),Tiempo) + " Bytes";
+}
+
+
